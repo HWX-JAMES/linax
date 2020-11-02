2bb40f8156d7aecac7836b44cc02de5f3aa135d8
HWX-JAMES <2510345299@qq.com>
2020-11-02 15:22:27 +0800

    week4

---
+++ week4/4-3.c
@@ -0,0 +1,16 @@
+#include "ch03.h"
+char buf1[]="abcdefghij";
+char buf2[]="1234567890";
+int main()
+{
+int fd;
+if((fd=open("file.hole.2",O_WRONLY|O_CREAT|O_APPEND,0644))<0)
+	err_exit("create error!");
+if(write(fd,buf1,10)!=10)
+	err_exit("write error!");
+if(lseek(fd,40,SEEK_SET)==-1)
+	err_exit("Lseek error!");
+if(write(fd,buf2,10)!=10)
+	err_exit("write error!");
+return 0;
+}
